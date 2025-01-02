759b8bd0d5111c7749be48672299436aeb884761
sik2 <bickwang123@gmail.com>
2025-01-02 11:20:09 +0900

    init

---
+++ src/main/java/com/ll/mutiChat/MutiChatApplication.java
@@ -0,0 +1,13 @@
+package com.ll.mutiChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutiChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutiChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/mutiChat/MutiChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.mutiChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutiChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
