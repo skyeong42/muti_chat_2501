add19f731de1d9044b5775e2b16f0a16fca98d4d
sik2 <bickwang123@gmail.com>
2025-01-03 12:25:27 +0900

    init chat message

--- src/main/java/com/ll/mutiChat/domain/chat/ChatMessage/service/ChatMessageService.java
+++ src/main/java/com/ll/mutiChat/domain/chat/ChatMessage/service/ChatMessageService.java
@@ -1,6 +1,8 @@
 package com.ll.mutiChat.domain.chat.ChatMessage.service;
 
+import com.ll.mutiChat.domain.chat.ChatMessage.entity.ChatMessage;
 import com.ll.mutiChat.domain.chat.ChatMessage.repository.ChatMessageRepository;
+import com.ll.mutiChat.domain.chat.ChatRoom.entity.ChatRoom;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +10,14 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ChatMessageService {
     private final ChatMessageRepository chatMessageRepository;
+
+    public void write(ChatRoom chatRoom, String writerName, String content) {
+        ChatMessage chatMessage = ChatMessage.builder()
+                .chatRoom(chatRoom)
+                .writerName(writerName)
+                .content(content)
+                .build();
+
+        chatMessageRepository.save(chatMessage);
+    }
 }
--- src/main/java/com/ll/mutiChat/global/initData/NotProd.java
+++ src/main/java/com/ll/mutiChat/global/initData/NotProd.java
@@ -1,5 +1,6 @@
 package com.ll.mutiChat.global.initData;
 
+import com.ll.mutiChat.domain.chat.ChatMessage.service.ChatMessageService;
 import com.ll.mutiChat.domain.chat.ChatRoom.entity.ChatRoom;
 import com.ll.mutiChat.domain.chat.ChatRoom.service.ChatRoomService;
 import org.springframework.boot.ApplicationRunner;
@@ -7,19 +8,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.util.stream.IntStream;
+
 @Configuration
 @Profile("!prod")
 public class NotProd {
 
     @Bean
-    public ApplicationRunner initNotProd(ChatRoomService chatRoomService) {
+    public ApplicationRunner initNotProd(ChatRoomService chatRoomService, ChatMessageService chatMessageService) {
         return args -> {
             ChatRoom chatRoom1 = chatRoomService.make("공부");
             ChatRoom chatRoom2 = chatRoomService.make("식사");
             ChatRoom chatRoom3 = chatRoomService.make("휴식");
 
-//            IntStream.rangeClosed(1, 100).forEach(num -> {
-//            });
+            IntStream.rangeClosed(1, 100).forEach(num -> {
+                chatMessageService.write(chatRoom1, "홍길동", "공부 메시지" + num);
+            });
 
         };
     }
